d3f493ca5f593445674424d85a785cb8d1ba3fcb
pokemon <pokemon@DESKTOP-HDC7UAU>
2016-10-27 14:24:10 +0800

    1

---
+++ Dai.Blog.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dai.Blog.Web.Startup))]
+namespace Dai.Blog.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+          
+        }
+    }
+}
