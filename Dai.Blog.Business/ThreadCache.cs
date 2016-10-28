72dfdcaf2791f378ccdbbc99278034861cf8627b
pokemon <pokemon@DESKTOP-USQ78RE>
2016-10-28 17:41:12 +0800

    1

---
+++ Dai.Blog.Business/ThreadCache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Dai.Blog
+{
+    public static class ThreadCache
+    {
+
+        public static QueryContext QueryContext
+        {
+            get
+            {
+                return HttpContext.Current.GetItem("_QueryContext", () => new QueryContext());
+            }
+        }
+    }
+}
