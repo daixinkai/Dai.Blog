cd39d1d9109f8c6d952f53be54a3cbfd5a73c8ec
pokemon <pokemon@DESKTOP-HDC7UAU>
2016-10-31 09:51:16 +0800

    1

--- Dai.Blog.Web/Controllers/AccountController.cs
+++ Dai.Blog.Web/Controllers/AccountController.cs
@@ -15,7 +15,7 @@ namespace Dai.Blog.Web.Controllers
         // GET: Account
         public async Task<ActionResult> Login()
         {
-
+             
             var claims = new List<Claim>();
             claims.Add(new Claim(ClaimTypes.Name, "pokemon"));
             claims.Add(new Claim(ClaimTypes.Role, "Users"));
@@ -23,7 +23,7 @@ namespace Dai.Blog.Web.Controllers
             var identity = new ClaimsIdentity(claims, "MyClaimLogin");
              
             //ClaimsPrincipal principal = new ClaimsPrincipal(identity);
-            HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = true }, identity);
+            HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties {  IsPersistent = true }, identity);
             //var claimsIdentity =
             //   await UserManager.CreateIdentityAsync(principal, DefaultAuthenticationTypes.ApplicationCookie);
             //AuthManager.SignOut();
